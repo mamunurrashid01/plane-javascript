586e495e1f3e535a1ecfe77b0fc9b7acd0582a44
mamunur-rashid <mdmamunurrashid.webdeveloper@gmail.com>
2024-02-24 23:23:23 +0600

    data type, remainde, toFixed, type conversion

--- conversion.js
+++ conversion.js
@@ -22,3 +22,31 @@ let isHappy = true;
 console.log(typeof isHappy); //the result is boolean;
 let defineFind;
 console.log(defineFind); //the result will undefine;
+
+let num1 = 0.1;
+let num2 = 0.2;
+let result = num1 + num2;
+console.log(result); //the will show 0.3000000004;
+//to fixed the problems
+console.log(result.toFixed(2)); //the result will show 2 digit after point, is 0.30 and will return string type data;
+
+//How to get  remainder from number
+
+let remainder = 50;
+console.log(remainder % 6); //the result is 2, that means we get remainder 2; '%' the sign is remainder;
+
+//There are 7 types of data type in  js
+/*
+those are :
+1.primitive data
+*Number;
+*String;
+*Undefine;
+*Null;
+*Boolean
+
+2.non-premative data
+*Object;
+*Array;
+*Function;
+*/
