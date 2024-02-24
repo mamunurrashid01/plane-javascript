d5f2d24f3b2b2475ad142f59972ad0e35210635a
mamunur-rashid <mdmamunurrashid.webdeveloper@gmail.com>
2024-02-25 00:07:01 +0600

    push, pop, get length of an array

--- module2/arrayandobject.js
+++ module2/arrayandobject.js
@@ -1,2 +1,11 @@
+//Array declaration
 let arr = [21, 12, 18, 80, 78, 88, 79, 89, 90, 65, 74];
 console.log(arr);
+// to add or include new element at last in a array;
+// arr.push(555);
+// console.log(arr);
+// to remove or delete an element from at last in a array;
+// arr.pop();
+
+// to get number of total element of an array we basically use '.length' method;
+console.log(arr.length);
