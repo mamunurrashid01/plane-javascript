bce91692ce9c19b9e6957c5f436c663b23023b40
mamunur-rashid <mdmamunurrashid.webdeveloper@gmail.com>
2024-02-25 07:32:38 +0600

    some example of array

--- module2/arrayindex.js
+++ module2/arrayindex.js
@@ -1,11 +1,22 @@
 let frinds = ["Mamunur", "Rashid", "Adib", "Adil", "Anower", "Faruk"];
 console.log(frinds);
-// get element by index
+// get/ find element by index
 console.log(frinds[3]); // use array name[index no.] to get element;
 
-//get index by elements to "indexOf" method
+//find index by elements to "indexOf" method
 console.log(frinds.indexOf("Rashid")); //use * indexOf * method; to get index number; if there was a index that has no exist then return value will -1;
 
 //set a new element by index
 frinds[5] = "Ahammed ali"; // set a new element * Ahmmed ali * by index 5;
 console.log(frinds);
+
+//some example index, get element, set new value
+let num = [12, 13, 34, 53, 65, 50, 62, 63, 64];
+let findIndex = num.indexOf(62);
+console.log(findIndex);
+
+let findValue = num[3];
+console.log(findValue);
+
+num[4] = 75;
+console.log(num);
