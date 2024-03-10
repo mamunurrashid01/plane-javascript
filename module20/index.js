f4e205d28edb14799c8123fe839bf4aeeb95b0fa
mamunur-rashid <mdmamunurrashid.webdeveloper@gmail.com>
2024-03-10 21:52:49 +0600

    module 20 finished

--- module20/index.js
+++ module20/index.js
@@ -54,3 +54,51 @@ let Newarrya = [
 ];
 let shoWresult = getArrFromNewArr(Newarrya);
 console.log(shoWresult);
+
+function addFactorial(fact) {
+  let multipli = 1;
+  for (let i = 1; i <= fact; i++) {
+    multipli *= i;
+  }
+  return multipli;
+}
+let inputNum = addFactorial(7);
+console.log(inputNum);
+
+function reverseFact(nub) {
+  let multip = 1;
+  for (let i = nub; i >= 1; i--) {
+    multip *= i;
+  }
+  return multip;
+}
+console.log(reverseFact(7));
+
+function whileLoop(num) {
+  let number = num;
+  let result = 1;
+  while (number >= 1) {
+    result *= number;
+    number--;
+  }
+  return result;
+}
+console.log(whileLoop(7));
+
+function findLeap(year) {
+  let leapYear = [];
+  for (let i = 0; i < year.length; i++) {
+    let getValue = year[i];
+    if (
+      (getValue % 4 == 0 && getValue % 100 !== 0) ||
+      (getValue % 100 == 0 && getValue % 400 == 0)
+    ) {
+      leapYear.push(getValue);
+    }
+    // console.log(getValue);
+  }
+  return leapYear;
+}
+let Year = [2023, 2024, 2028, 2030];
+let callFunc = findLeap(Year);
+console.log(callFunc);
